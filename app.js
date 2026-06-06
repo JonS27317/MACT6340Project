94a231a2278c54658ecedabcca86d5d340fb7337
JonS27317 <jonschroeder@smu.edu>
2026-06-05 23:03:17 -0500

    Completed ejs and initial mySQL module

--- app.js
+++ app.js
@@ -3,15 +3,34 @@ import express from 'express';
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js"
 dotenv.config();
+let data = ["Project 1", "Project 2", "Project 3"];
 
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs")
 app.use(express.json());
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//     res.send('hello world');
-// });
+app.get('/', (req, res) => {
+    res.render("index.ejs")
+});
+
+
+app.get("/project/:id", (req, res) => {
+    let id = req.params.id;
+    if (id > data.length) {
+        throw new Error("No project with that ID");
+    }
+    res.render("project.ejs", { projectArray: data, which: id });
+});
+
+app.get('/projects', (req, res) => {
+    res.render("projects.ejs", { projectArray: data })
+});
+
+app.get('/contact', (req, res) => {
+    res.render("contact.ejs")
+});
 
 app.post("/mail", async (req, res) => {
     await utils
@@ -24,6 +43,10 @@ app.post("/mail", async (req, res) => {
         });
 });
 
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.render("error.ejs");
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
