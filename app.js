671c8e2d6f6c7db8d9bcdffbda845e0ecf2fb7d5
JonS27317 <jonschroeder@smu.edu>
2026-06-29 22:32:58 -0500

    changed case again to match online repo

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 import express from 'express';
 import dotenv from "dotenv";
 //this is why case is important
-import * as utils from "./utils/utils.js"
+import * as utils from "./Utils/utils.js"
 import cors from "cors";
 
 dotenv.config();
-import * as db from './utils/database.js';
+import * as db from './Utils/database.js';
 let data = ["Project 1", "Project 2", "Project 3"];
 let projects = [];
 
